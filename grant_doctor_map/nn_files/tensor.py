b20f4fd89e9d471fd3c0bd4ae082fa9a33cdbc8a
shearere2 <shearere2@duq.edu>
2024-04-21 14:07:07 -0400

    Removed files that were in the incorrect location.

--- grant_doctor_map/nn_files/tensor.py
+++
@@ -1,9 +0,0 @@
-"""A tensor, which sounds super complicated, is nothing more than an
-n-dimensional array. A 1-d array or 1-d tensor is a vector. A 2-d
-tensor is a matrix.
-
-Tensorflow == matrix math
-
-ndarray is the base of numpy
-"""
-from numpy import ndarray as Tensor
